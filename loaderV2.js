56d5e3f037a229ad829fedd76e1186c8890475dd
qxionr <118845758+qxionr@users.noreply.github.com>
2025-08-15 21:27:35 +0100

    clean Loader

--- loaderV2.js
+++ loaderV2.js
@@ -13,40 +13,6 @@ Client.getMinecraft()
   .getWindow()
   .setTitle("Client " + global.Version + ` - ${Player.getName()}`);
 
-/* Dependencies */
-import { Vector3 } from "./Dependencies/BloomCore/Vector3.js";
-import { raytraceBlocks } from "./Dependencies/BloomCore/RaytraceBlocks.js";
-
-/* Utility */
-import "./Utility/Config.js";
-import { Guis } from "./Utility/Inventory";
-import { MathUtils } from "./Utility/Math.js";
-import { MiningUtils } from "./Utility/MiningUtils.js";
-import "./Utility/Misc.js";
-import { Keybind } from "./Utility/Keybinding.js";
-import { Popup } from "./Utility/PopUpMenu";
-import { Prefix } from "./Utility/Prefix";
-import { RayTrace } from "./Utility/Raytrace.js";
-import { Rotations } from "./Utility/Rotations.js";
-import { registerEventSB } from "./Utility/SkyblockEvents.js";
-import { Conversions } from "./Utility/TimeConversion";
-import { Timers } from "./Utility/Timing";
-import { Mouse } from "./Utility/Ungrab";
-import { Utils } from "./Utility/Utils.js";
-import { Webhook } from "./Utility/Webhooks.js";
-// do raytrace
-
-/* DataClasses */
-import { ItemObject } from "./Utility/DataClasses/ItemObject.js";
-import { Vector } from "./Utility/DataClasses/Vec.js";
-
-/* GUI */
-import "./GUI/Gui.js";
-
-/* Macros */
-
-/* QOL */
-
 /* Mixins */
 
 import { horizontalConnectingBlock_modifyPaneHitbox } from "./mixins.js";
