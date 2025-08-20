3680c9d4f1f1793c6d4980295d57473ea61538b9
qxionr <118845758+qxionr@users.noreply.github.com>
2025-08-21 00:04:14 +0100

    Push some things

--- GUI/GuiDraw.js
+++ GUI/GuiDraw.js
@@ -255,43 +255,17 @@ const categoryManager = global.createCategoriesManager({
 });
 
 const drawGUI = (mouseX, mouseY) => {
-  const elapsed = Date.now() - openStartTime;
-  const progress = clamp(elapsed / ANIMATION_DURATION, 0, 1);
-
-  const backgroundHeight = rectangles.Background.height;
-  const leftPanelHeight = rectangles.LeftPanel.height;
-  const rightPanelHeight = rectangles.RightPanel.height;
-
-  const currentBackgroundHeight = backgroundHeight * progress;
-  const currentLeftPanelHeight = leftPanelHeight * progress;
-  const currentRightPanelHeight = rightPanelHeight * progress;
-
-  const animatedBackground = {
-    ...rectangles.Background,
-    height: currentBackgroundHeight,
-  };
-  const animatedLeftPanel = {
-    ...rectangles.LeftPanel,
-    height: currentLeftPanelHeight,
-  };
-  const animatedRightPanel = {
-    ...rectangles.RightPanel,
-    height: currentRightPanelHeight,
-  };
-
   Client.getMinecraft().gameRenderer.renderBlur();
 
-  drawRoundedRectangleWithBorder(animatedBackground);
+  drawRoundedRectangleWithBorder(rectangles.Background);
   drawRoundedRectangleWithGradientOutline(
-    animatedLeftPanel,
+    rectangles.LeftPanel,
     GRADIENT_TOP_COLOR,
     GRADIENT_BOTTOM_COLOR
   );
-  drawRoundedRectangleWithBorder(animatedRightPanel);
+  drawRoundedRectangleWithBorder(rectangles.RightPanel);
 
-  if (progress >= 0.5) {
-    categoryManager.draw(mouseX, mouseY);
-  }
+  categoryManager.draw(mouseX, mouseY);
 };
 
 myGui.registerDraw(drawGUI);
