f67fb25065257e91b7be58056270f5ddadddc865
qxionr <118845758+qxionr@users.noreply.github.com>
2025-08-20 22:29:18 +0100

    Gui Blur

---
+++ GUI/Registries.js
@@ -0,0 +1,10 @@
+/* Categories */
+global.Categories.addCategory("Mining");
+
+//global.Categories.addCategory("Foraging");
+
+global.Categories.addCategory("World");
+
+global.Categories.addCategory("Quality of Life");
+
+global.Categories.addCategory("Mining");
