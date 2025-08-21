4ba9b5b050f29dc50e5738c4ce1c8e663e848aae
qxionr <118845758+qxionr@users.noreply.github.com>
2025-08-21 16:37:14 +0100

    fix fps </3 left dampening

--- GUI/Registries.js
+++ GUI/Registries.js
@@ -1,52 +1,40 @@
-const { addCategory, addCategoryItem, addToggle, addSlider, addSubCategory } =
+const { addCategoryItem, addToggle, addSlider, addSubCategory } =
   global.Categories;
 
 /* MINING */
-addCategory("Modules");
-
 addCategoryItem(
-  "Modules",
   "Mining",
   "Mining Bot",
   "Mines blocks of multiple types on all islands"
 );
 
 addCategoryItem(
-  "Modules",
   "Mining",
   "Gemstone Macro",
   "Mines gemstones in the Crystal Hollows"
 );
 
-addCategoryItem(
-  "Modules",
-  "Mining",
-  "Ore Macro",
-  "Mines ores across all islands"
-);
+addCategoryItem("Mining", "Ore Macro", "Mines ores across all islands");
 
 addCategoryItem(
-  "Modules",
   "Mining",
   "Commission Macro",
   "Completes Commissions in the Dwarven Mines"
 );
 
 addCategoryItem(
-  "Modules",
   "Mining",
   "Glacite Commission Macro",
   "Completes Commissions in the Glacite Tunnels"
 );
 
 addCategoryItem(
-  "Modules",
   "Mining",
   "Excavator Macro",
   "Automatically does the excavator task"
 );
 
-addCategoryItem("Modules", "Stuffs", "Mining Bots", "stuffs");
+addCategoryItem("Stuffs", "Mining Bots", "stuffs");
 
 /*addCategoryItem(
   "Modules",
