6e2be2f8f620921a39f2e41f99902b7d66642a4f
Nathan <209938737+quiteboringv2@users.noreply.github.com>
2025-08-22 00:11:15 -0400

    update and cleanup mixins

--- mixins.js
+++ mixins.js
@@ -1,31 +1,20 @@
-const horizontalConnectingBlockMixin = new Mixin(
-  "net.minecraft.block.HorizontalConnectingBlock"
-);
-
-export const horizontalConnectingBlock_modifyPaneHitbox =
-  horizontalConnectingBlockMixin.inject({
-    method: "getOutlineShape",
-    at: new At({ value: "HEAD" }),
-    cancellable: true,
-  });
-
-//const seaPickleBlockMixin = new Mixin("net.minecraft.block.SeaPickleBlock");
-
-//export const fullPickle = seaPickleBlockMixin.inject({
-//  method: "getOutlineShape",
-//  at: new At({ value: "HEAD" }),
-//  cancellable: true,
-//});
-
-//const seaGrassMixin = new Mixin("net.minecraft.block.SeagrassBlock");
-
-//export const emptyGrass = seaGrassMixin.inject({
-///  method: "getOutlineShape",
-//  at: new At({ value: "HEAD" }),
-//  cancellable: true,
-//});
-
+const horizontalConnectingBlockMixin = new Mixin("net.minecraft.block.HorizontalConnectingBlock");
+const seaPickleBlockMixin = new Mixin("net.minecraft.block.SeaPickleBlock");
 const partBlockMixin = new Mixin("net.minecraft.block.AbstractPlantPartBlock");
+const seaGrassMixin = new Mixin("net.minecraft.block.SeagrassBlock");
+const tallSeaGrassMixin = new Mixin("net.minecraft.block.TallSeagrassBlock");
+
+export const fullStainedGlassPane = horizontalConnectingBlockMixin.inject({
+  method: "getOutlineShape",
+  at: new At({ value: "HEAD" }),
+  cancellable: true,
+});
+
+export const fullPickle = seaPickleBlockMixin.inject({
+  method: "getOutlineShape",
+  at: new At({ value: "HEAD" }),
+  cancellable: true,
+});
 
 export const emptyKelp = partBlockMixin.inject({
   method: "getOutlineShape",
@@ -33,10 +22,14 @@ export const emptyKelp = partBlockMixin.inject({
   cancellable: true,
 });
 
-//const plantBlockMixin = new Mixin("net.minecraft.block.PlantBlock");
+export const emptyGrass = seaGrassMixin.inject({
+  method: "getOutlineShape",
+  at: new At({ value: "HEAD" }),
+  cancellable: true,
+});
 
-//export const emptyPlants = plantBlockMixin.inject({
-//  method: "getOutlineShape",
-//  at: new At({ value: "HEAD" }),
-//  cancellable: true,
-//});
+export const emptyTallGrass = tallSeaGrassMixin.inject({
+  method: "getOutlineShape",
+  at: new At({ value: "HEAD" }),
+  cancellable: true,
+});
