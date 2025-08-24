d3fc79fd0d5a56abb4c7a11437fef907e9a83f00
EpsilonPhoenix <dontemailme@stop.com>
2025-08-24 18:31:43 -0400

    other than vector3, this is basically entirely ai cuz i have no idea what's wrong. this probably wont work but how would i know? im too lazy to test it

--- Dependencies/BloomCore/Vector3.js
+++ Dependencies/BloomCore/Vector3.js
@@ -1,4 +1,4 @@
-class Vector3Class {
+export class Vector3 {
   static fromCoords = (x0, y0, z0, x1, y1, z1) =>
     new Vector3(x1 - x0, y1 - y0, z1 - z0);
 
@@ -16,18 +16,10 @@ class Vector3Class {
     this.z = z ?? 0;
   }
 
-  /**
-   * Returns the x, y and z components of the vector as [x, y, z]
-   * @returns {[Number, Number, Number]}
-   */
   getComponents() {
     return [this.x, this.y, this.z];
   }
 
-  /**
-   * Subtracts a vector from this vector.
-   * @param {Vector3} vector3
-   */
   subtract(vector3) {
     return new Vector3(
       this.x - vector3.x,
@@ -36,90 +28,42 @@ class Vector3Class {
     );
   }
 
-  /**
-   * Adds another Vector3 or 3d coordinate to this vector.
-   * @param {Vector3 | [Number, Number, Number]} vector3
-   */
   add(vector3) {
     if (vector3 instanceof Vector3) {
-      this.x += vector3.x;
-      this.y += vector3.y;
-      this.z += vector3.z;
-      return this;
+      return new Vector3(this.x + vector3.x, this.y + vector3.y, this.z + vector3.z);
     }
-    this.x += vector3[0];
-    this.y += vector3[1];
-    this.z += vector3[2];
-    return this;
+    return new Vector3(this.x + vector3[0], this.y + vector3[1], this.z + vector3[2]);
   }
 
-  /**
-   * Returns the dot product of two vectors.
-   * @param {Vector3} vector3
-   * @returns {Number}
-   */
   dotProduct(vector3) {
-    let [x1, y1, z1] = this.getComponents();
-    let [x2, y2, z2] = vector3.getComponents();
-    return x1 * x2 + y1 * y2 + z1 * z2;
+    return this.x * vector3.x + this.y * vector3.y + this.z * vector3.z;
   }
 
-  /**
-   * Returns the cross product of two vectors
-   * @param {Vector3} vector3
-   * @returns {Vector3}
-   */
   crossProduct(vector3) {
-    let [x1, y1, z1] = this.getComponents();
-    let [x2, y2, z2] = vector3.getComponents();
     return new Vector3(
-      y1 * z2 - z1 * y2,
-      -(x1 * z1 - z1 * x2),
-      x1 * y2 - y1 * x2
+      this.y * vector3.z - this.z * vector3.y,
+      this.z * vector3.x - this.x * vector3.z,
+      this.x * vector3.y - this.y * vector3.x
     );
   }
 
-  /**
-   * Gets the length of the vector
-   * @returns {Number}
-   */
   getLength() {
     return Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2);
   }
-
-  /**
-   * Gets the angle between this and another vector in radians
-   * @param {Vector3} vector3
-   * @returns {Number}
-   */
+  
   getAngleRad(vector3) {
     return Math.acos(
       this.dotProduct(vector3) / (this.getLength() * vector3.getLength())
     );
   }
 
-  /**
-   * Gets the angle between this and another vector in degrees
-   * @param {Vector3} vector3
-   * @returns {Number}
-   */
   getAngleDeg(vector3) {
     return (180 / Math.PI) * this.getAngleRad(vector3);
   }
 
-  /**
-   * Gets the equation for the plane from three points.
-   * @param {Number[]} point1
-   * @param {Number[]} point2
-   * @param {Number[]} point3
-   * @returns {Number[]} - An array of numbers containing the [x, y, z, extra].
-   */
   getPlaneEquation(point1, point2, point3) {
-    let [p1x, p1y, p1z] = point1;
-    let [p2x, p2y, p2z] = point2;
-    let [p3x, p3y, p3z] = point3;
-    let d1 = new Vector3(p2x - p1x, p2y - p1y, p2z - p1z);
-    let d2 = new Vector3(p3x - p1x, p3y - p1y, p3z - p1z);
+    let d1 = new Vector3(point2[0] - point1[0], point2[1] - point1[1], point2[2] - point1[2]);
+    let d2 = new Vector3(point3[0] - point1[0], point3[1] - point1[1], point3[2] - point1[2]);
     let normal = d1.crossProduct(d2);
     return [
       ...normal.getComponents(),
@@ -127,77 +71,53 @@ class Vector3Class {
     ];
   }
 
-  /**
-   * Normalizes the vector
-   * @returns {Vector3}
-   */
   normalize() {
     const len = this.getLength();
-    this.x = this.x / len;
-    this.y = this.y / len;
-    this.z = this.z / len;
-    return this;
+    if (len === 0) return new Vector3(0, 0, 0); // Prevent division by zero
+    return new Vector3(this.x / len, this.y / len, this.z / len);
   }
 
+  multiply(factor) {
+    return new Vector3(this.x * factor, this.y * factor, this.z * factor);
+  }
+
+  rotate(degrees, reverse = false) {
+    if (reverse) degrees = (360 - degrees) % 360;
+    let newX = this.x;
+    let newZ = this.z;
+    switch (degrees) {
+      case 90:
+        [newX, newZ] = [this.z, -this.x];
+        break;
+      case 180:
+        [newX, newZ] = [-this.x, -this.z];
+        break;
+      case 270:
+        [newX, newZ] = [-this.z, this.x];
+        break;
+    }
+    return new Vector3(newX, this.y, newZ);
+  }
+  
   getYaw() {
-    this.normalize();
-    return (180 / Math.PI) * -Math.atan2(this.x, this.z);
+    const normalized = this.normalize();
+    return (180 / Math.PI) * -Math.atan2(normalized.x, normalized.z);
   }
 
   getPitch() {
-    this.normalize();
-    return (180 / Math.PI) * -Math.asin(this.y);
+    const normalized = this.normalize();
+    return (180 / Math.PI) * -Math.asin(normalized.y);
   }
 
   toString() {
     return `Vector3(x=${this.x},y=${this.y},z=${this.z})`;
   }
-  /**
-   * Rotates a vector in the clockwise direction. Returns itself to enable chaining methods.
-   * @param {Number} degrees - How much to rotate the vector by. Only accepts degrees which are a multiple of 90.
-   * @returns {Vector} - The rotated version of this vector.
-   */
-  rotate(degrees, reverse = false) {
-    if (reverse) degrees = (360 - degrees) % 360;
-    switch (degrees) {
-      case 90:
-        [this.x, this.z] = [this.z, -this.x];
-        break;
-      case 180:
-        [this.x, this.z] = [-this.x, -this.z];
-        break;
-      case 270:
-        [this.x, this.z] = [-this.z, this.x];
-        break;
-    }
-    return this;
-  }
-  multiply(factor) {
-    this.x *= factor;
-    this.y *= factor;
-    this.z *= factor;
-    return this;
-  }
 
-  getX() {
-    return this.x;
-  }
+  getX() { return this.x; }
+  getY() { return this.y; }
+  getZ() { return this.z; }
 
-  getY() {
-    return this.y;
-  }
-
-  getZ() {
-    return this.z;
-  }
-
-  /**
-   * Creates a new Vector3 object identical to this one.
-   * @returns {Vector3} - A new Vector3 with the same x, y and z component.
-   */
   copy() {
     return new Vector3(this.x, this.y, this.z);
   }
 }
-
-export const Vector3 = new Vector3Class();
