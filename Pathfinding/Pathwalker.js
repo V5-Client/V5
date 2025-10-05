61993fb6cdebf4da9d639e44f61675dc53568ffc
qxionr <dontemailme@stop.com>
2025-10-05 11:45:25 +0100

    update pathwalker rendering

--- Pathfinding/Pathwalker.js
+++ Pathfinding/Pathwalker.js
@@ -1,6 +1,6 @@
 import request from 'requestV2';
-import RendererMain from '../Rendering/RendererMain';
-import { Links } from '../Utility/Constants';
+import RenderUtils from '../Rendering/RendererUtils';
+import { Links, Vec3d } from '../Utility/Constants';
 import { Rotations } from '../Utility/Rotations';
 import { RayTrace } from '../Utility/Raytrace';
 import './Connection';
@@ -225,34 +225,41 @@ function renderPath() {
     movementState.splinePath.forEach((node, i) => {
         const isCurrent = i === movementState.currentNodeIndex;
         const color = isCurrent
-            ? new Color(1, 1, 0)
+            ? [255, 255, 0, 100] // change these colors to fit ur desires
             : i < movementState.currentNodeIndex
-            ? new Color(0.3, 0.3, 0.3, 0.3)
-            : new Color(0, 1, 0, 0.5);
-        RendererMain.drawWaypoint(
-            new Vec3i(node.x, node.y, node.z),
+            ? [76, 76, 76, 76]
+            : [0, 255, 0, 127];
+
+        RenderUtils.drawStyledBox(
+            new Vec3d(node.x, node.y, node.z),
             isCurrent,
-            color
+            isCurrent,
+            5,
+            false
         );
     });
 
     keyNodes.forEach((node) =>
-        RendererMain.drawWaypoint(
-            new Vec3i(node.x, node.y, node.z),
-            true,
-            new Color(1, 0, 0, 0.8)
+        RenderUtils.drawStyledBox(
+            new Vec3d(node.x, node.y, node.z),
+            [255, 0, 0, 204],
+            [255, 0, 0, 204],
+            5,
+            false
         )
     );
 
     if (movementState.isWalking && movementState.targetPoint) {
-        RendererMain.drawWaypoint(
-            new Vec3i(
+        RenderUtils.drawStyledBox(
+            new Vec3d(
                 movementState.targetPoint.x,
                 movementState.targetPoint.y,
                 movementState.targetPoint.z
             ),
-            true,
-            new Color(0, 1, 1)
+            [0, 255, 255, 255],
+            [0, 255, 255, 255],
+            5,
+            false
         );
     }
 }
