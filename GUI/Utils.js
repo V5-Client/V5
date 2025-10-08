94ca8cfdcbe7f59ec76a62b9b2dd4653407e8f72
EpsilonPhoenix <dontemailme@stop.com>
2025-10-07 20:10:29 -0400

    replace ChatLib.chat everywhere ¯\_(ツ)_/¯

--- GUI/Utils.js
+++ GUI/Utils.js
@@ -305,7 +305,7 @@ export const returnDiscord = () => {
                 }
 
                 if (!data || !data.discord || !data.discord.avatar) {
-                    ChatLib.chat(
+                    Chat.Message(
                         'Failed to download your Discord pfp: Invalid data format.'
                     );
                     return;
