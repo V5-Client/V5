a1d55739a121ffb5d534d6f0428c26e9c9077a26
qxionr <118845758+qxionr@users.noreply.github.com>
2025-10-21 20:39:15 +0100

    forgot to push config thingy

--- Pathfinding2.0/PathConfig.js
+++ Pathfinding2.0/PathConfig.js
@@ -1,6 +1,47 @@
 // Centralized configuration for Pathfinding module
-
 import { Chat } from '../Utility/Chat';
+import { ModuleBase } from '../Utility/ModuleBase';
+
+import './PathCommands';
+import './Connection';
+
+class PathRegistry extends ModuleBase {
+    constructor() {
+        super({
+            name: 'Pathfinding',
+            subcategory: 'Core',
+            description: 'Pathfinding Utilities',
+            tooltip: 'Pathfinding Utilities',
+            showEnabledToggle: false,
+        });
+
+        this.addToggle(
+            'Pathfinding Debug',
+            (value) => {
+                setPathfindingDebug(value);
+            },
+            'Enables pathfinding debug mode'
+        );
+
+        this.addToggle(
+            'Render Key Nodes',
+            (value) => {
+                setRenderKeyNodes(value);
+            },
+            'Renders the key nodes of the path'
+        );
+
+        this.addToggle(
+            'Toggle Sprint',
+            (value) => {
+                setToggleSprint(value);
+            },
+            'Toggles sprinting while walking'
+        );
+    }
+}
+
+new PathRegistry();
 
 let renderKeyNodes = false;
 let toggleSprint = false;
@@ -19,7 +60,7 @@ export function getPathfindingDebug() {
 
 export function PathfindingMessages(msg) {
     if (!getPathfindingDebug()) return;
-    Chat.message(msg);
+    Chat.message('&7Pathfinder: ' + msg);
 }
 
 export function setRenderKeyNodes(value) {
