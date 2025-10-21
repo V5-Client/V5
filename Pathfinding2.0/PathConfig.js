20565b3ca79bcec72770219851e90fcd48298c7f
qxionr <118845758+qxionr@users.noreply.github.com>
2025-10-21 21:53:18 +0100

    fix pathfinder module duplicates

--- Pathfinding2.0/PathConfig.js
+++ Pathfinding2.0/PathConfig.js
@@ -1,47 +1,5 @@
 // Centralized configuration for Pathfinding module
 import { Chat } from '../Utility/Chat';
-import { ModuleBase } from '../Utility/ModuleBase';
-
-import './PathCommands';
-import './Connection';
-
-class PathRegistry extends ModuleBase {
-    constructor() {
-        super({
-            name: 'Pathfinding',
-            subcategory: 'Core',
-            description: 'Pathfinding Utilities',
-            tooltip: 'Pathfinding Utilities',
-            showEnabledToggle: false,
-        });
-
-        this.addToggle(
-            'Pathfinding Debug',
-            (value) => {
-                setPathfindingDebug(value);
-            },
-            'Enables pathfinding debug mode'
-        );
-
-        this.addToggle(
-            'Render Key Nodes',
-            (value) => {
-                setRenderKeyNodes(value);
-            },
-            'Renders the key nodes of the path'
-        );
-
-        this.addToggle(
-            'Toggle Sprint',
-            (value) => {
-                setToggleSprint(value);
-            },
-            'Toggles sprinting while walking'
-        );
-    }
-}
-
-new PathRegistry();
 
 let renderKeyNodes = false;
 let toggleSprint = false;
