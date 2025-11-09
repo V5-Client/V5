1005203bf2f828e5bbc85f2ce30d2640ede24984
qxionr <dontemailme@stop.com>
2025-11-09 16:08:26 -0500

    feat: mouse & gamerenderer mixin

--- Utility/Ungrab.js
+++ Utility/Ungrab.js
@@ -1,31 +1,26 @@
-import { LockCursor, IsCursorLocked, OnCursorPos } from '../Mixins/UngrabMixin';
+import { IsCursorLocked } from '../Mixins/UngrabMixin';
 import { attachMixin } from './AttachMixin';
 
 class ungrabClass {
     constructor() {
         this.ungrabbed = true;
         this.Regrab();
+        
+        register('command', () => {
+            if (this.ungrabbed) {
+                this.Regrab();
+            } else {
+                this.Ungrab();
+            }
+        }).setName("hi")
     }
 
     Ungrab() {
         if (this.ungrabbed) return;
 
-        attachMixin(LockCursor, 'LockCursor', (instance, cir) => {
-            if (!this.ungrabbed) return;
-            cir.cancel();
-        });
-
         attachMixin(IsCursorLocked, 'IsCursorLocked', (instance, cir) => {
-            if (!this.ungrabbed) return;
-
-            if (Client.getMinecraft()?.currentScreen == null) {
-                cir.setReturnValue(true);
-                cir.cancel();
-            }
-        });
-
-        attachMixin(OnCursorPos, 'OnCursorPos', (instance, cir) => {
-            if (!this.ungrabbed) return;
+            Client.getMinecraft().mouse.unlockCursor();
+            cir.setReturnValue(false);
             cir.cancel();
         });
 
@@ -34,9 +29,7 @@ class ungrabClass {
 
     Regrab() {
         if (!this.ungrabbed) return;
-        attachMixin(LockCursor, 'LockCursor', (instance, cir) => {});
         attachMixin(IsCursorLocked, 'IsCursorLocked', (instance, cir) => {});
-        attachMixin(OnCursorPos, 'OnCursorPos', (instance, cir) => {});
         this.ungrabbed = false;
     }
 }
