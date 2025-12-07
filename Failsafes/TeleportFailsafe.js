efaa5e9c90d49cb9a235061837abbbc672da54ef
oblongboot <81798709+oblongboot@users.noreply.github.com>
2025-12-07 22:29:22 +0000

    fix: allow for different module names instead of `V5`, and use packet.change().position() instead of stupid regex thing

--- Failsafes/TeleportFailsafe.js
+++ Failsafes/TeleportFailsafe.js
@@ -13,30 +13,15 @@ class TeleportFailsafe extends Failsafe {
 
     registerTPListeners() {
         register("packetReceived", (packet) => {
-            if (packet instanceof net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket) {
-                const fromX = Player.getX();
-                const fromY = Player.getY();
-                const fromZ = Player.getZ();
+            const fromX = Player.getX();
+            const fromY = Player.getY();
+            const fromZ = Player.getZ();
+            const newX = packet.change().position().x
+            const newY = packet.change().position().y
+            const newZ = packet.change().position().z
 
-                try {
-                    const str = packet.toString(); // this is SO cooked but it sadly works, nothign else would and the mappings site is down soo, maybe change later? idk
-                    const match = str.match(/position=\(\s*([-\d\.]+),\s*([-\d\.]+),\s*([-\d\.]+)\s*\)/);
-                    
-                    if (match) {
-                        const toX = parseFloat(match[1]);
-                        const toY = parseFloat(match[2]);
-                        const toZ = parseFloat(match[3]);
-                        
-                        this.onTrigger(fromX.toFixed(2), fromY.toFixed(2), fromZ.toFixed(2), toX.toFixed(2), toY.toFixed(2), toZ.toFixed(2));
-                    } else {
-                        Chat.debugMessage("regex failed matching packet :( you did get teleported tho so like, not very good i think")
-                    }
-                } catch (e) {
-                    Chat.debugMessage("error reading teleport packet: " + e);
-                    Chat.debugMessage("packet: " + packet.toString());
-                }
-            }
-        });
+            this.onTrigger(fromX.toFixed(2), fromY.toFixed(2), fromZ.toFixed(2), newX.toFixed(2), newY.toFixed(2), newZ.toFixed(2));
+        }).setFilteredClass(net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket)
     }
 
     onTrigger(fromX, fromY, fromZ, toX, toY, toZ) {
