64a981510a4bb8b96d38288cec2da82e6d7d27a0
qxionr <dontemailme@stop.com>
2025-12-09 16:49:14 +0000

    feat: add blacklisted words to chatmentionfailsafe

--- Failsafes/impl/ChatMentionFailsafe.js
+++ Failsafes/impl/ChatMentionFailsafe.js
@@ -9,14 +9,29 @@ class ChatMentionFailsafe extends Failsafe {
         this.ignore = false;
         this.settings = getFailsafeSettings("Chat Mention");
         this.registerChatListeners();
+        this.blacklistedWords = [
+            "macro",
+            "report",
+            "wdr",
+            "cheating",
+            "cheater"
+          //  `${Player.getName()}`, // add later when i can stop false positives
+        ]
     }
 
     registerChatListeners() {
-        register("chat", (message) => {
-            const playerName = Player.getName();
-            if (!message.toLowerCase().includes(playerName.toLowerCase())) return /// TODO: add custom failsafe words list?
-            Chat.message("You were mentioned in chat!"); 
+        register("chat", (msg) => {
+            if (!this.isBad(msg).isBlocked) return
+            Chat.message("Detected blacklisted word! (" + this.isBad(msg).blockedWord + ")"); 
         }).setCriteria(/(.+)/);
+    }
+
+    isBad(msg) {
+        let found = null;
+        const lower = msg.toLowerCase();
+        const isBlocked = this.blacklistedWords.some(word => { if (lower.includes(word.toLowerCase())) { found = word; return true; } return false; });
+
+        return { isBlocked: isBlocked, blockedWord: found };
     }
 
     onTrigger() {}
