dba7362bf7637830d4aa4f3ceefbb6c3defe83eb
oblongboot <81798709+oblongboot@users.noreply.github.com>
2025-12-11 19:38:32 +0000

    fix: playergreiffailsafe's player nearby failsafe

--- Failsafes/FailsafeUtils.js
+++ Failsafes/FailsafeUtils.js
@@ -5,15 +5,16 @@ let failsafeIntensity = 0;
 function getFailsafeSettings(name) {
     const modulesDir = new File("./config/ChatTriggers/modules");
     const V5ConfigFile = new File(`${modulesDir}/V5Config/config.json`);
-    if (!V5ConfigFile.exists()) { // TODO: make this not be fucking retarded and find another way
+    if (!V5ConfigFile.exists()) {
         console.log("V5Config not found, this shouldnt happen!");
-        return; // (importing module causes it to double render therefore i have to do this for now?)
+        return;
     }
 
     const config = JSON.parse(FileLib.read(V5ConfigFile.getAbsolutePath()));
     const FailsafeReactionTime = config["Failsafes"]["Failsafe Detection Delay (ms)"]
     const isEnabled = config["Failsafes"][`${name} Failsafe`]
-    return {isEnabled: isEnabled, FailsafeReactionTime: FailsafeReactionTime};
+    const playerProximityDistance = config["Failsafes"]["Player Proximity Distance"]
+    return {isEnabled: isEnabled, FailsafeReactionTime: FailsafeReactionTime, playerProximityDistance: playerProximityDistance};
 }
 
 function incrementFailsafeIntensity(amt) {
