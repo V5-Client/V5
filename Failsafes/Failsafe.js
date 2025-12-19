085a1e7d635067554581c11bf5cb9f5a04f1273f
qxionr <118845758+qxionr@users.noreply.github.com>
2025-12-19 00:55:51 +0000

    shitty failsafe utils stuff, still wip

--- Failsafes/Failsafe.js
+++ Failsafes/Failsafe.js
@@ -1,9 +1,11 @@
 import { AlertUtils } from './AlertUtils.js';
+import { Chat } from '../utils/Chat.js';
 
 export class Failsafe {
     constructor() {
         register('command', () => {
-            AlertUtils.playSound();
+            AlertUtils.isAlerting = true;
+            AlertUtils.triggerReaction();
         }).setName('trigger');
     }
 
