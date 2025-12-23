4f9835eff2303bc2b28261a89146a7a91a3825eb
qxionr <dontemailme@stop.com>
2025-12-23 22:43:15 +0000

    Vertical crops farming macro, ive only tested this on netherwart

--- Modules/Farming/FarmingMacro.js
+++ Modules/Farming/FarmingMacro.js
@@ -4,13 +4,40 @@ import { ModuleBase } from '../../utils/ModuleBase';
 import { Rotations } from '../../utils/player/Rotations';
 import { Mouse } from '../../utils/Ungrab';
 import { Utils } from '../../utils/Utils';
+import { Guis } from '../../utils/player/Inventory';
+import { Keybind } from '../../utils/player/Keybinding';
+
+const FARMING_DATA = [
+    {
+        name: 'Vertical NetherWart / Potato / Wheat / Carrot',
+        registry: ['minecraft:nether_wart', 'minecraft:potatoes', 'minecraft:wheat', 'minecraft:carrots'],
+        avgBPS: 2.19,
+        pitch: 3,
+    },
+    {
+        name: "MelonKingDe's Melon / Pumpkin",
+        registry: ['minecraft:melon', 'minecraft:carved_pumpkin'],
+        blockCheck: 1,
+        avgBPS: 2.19,
+        pitch: -59.2,
+    },
+];
+
+// how i decided to make this work
+// big decision making :sob:
+// decide roations and item
+// then decide Movement only tells Idlechecks where to go and what to do
+// if you plan on adding other crops follow this pattern
+
+// todo : rewarp, check block  metadata for crop type when sides is balanced out etc
+
 class FarmingMacro extends ModuleBase {
     constructor() {
         super({
             name: 'Farming Macro',
             subcategory: 'Farming',
             description: 'Automates farming for various crops.',
-            tooltip: 'WIP farming macro, requires farm layouts. Make a suggestion for more farm layouts.',
+            tooltip: 'WIP farming macro',
             showEnabledToggle: false,
             autoDisableOnWorldUnload: true,
         });
@@ -19,16 +46,27 @@ class FarmingMacro extends ModuleBase {
             WAITING: 0,
             SCANFORCROP: 1,
             DECIDEROTATION: 2,
-            MOVEMENT: 3,
+            DECIDEITEM: 3,
+            DECIDEMOVEMENT: 4,
+            IDLECHECKS: 5,
         };
 
         this.state = this.STATES.WAITING;
         this.farmAxis = null;
+        this.movementKey = null;
+        this.ignoreKey = null;
+        this.inAir = false;
 
         this.bindToggleKey();
-        this.addMultiToggle('Crop', ['Vertical Netherwart', 'Vertical Potato', 'Vertical Wheat', 'Vertical Carrot', 'Melon/Pumpkin'], true, (v) => {
-            this.CROPS = v;
-        });
+
+        this.addMultiToggle(
+            'Crop',
+            FARMING_DATA.map((data) => data.name),
+            true,
+            (v) => {
+                this.CROPS = v;
+            }
+        );
 
         this.on('tick', () => {
             if (Utils.area() !== 'Garden') {
@@ -93,8 +131,25 @@ class FarmingMacro extends ModuleBase {
                     }
                     break;
                 case this.STATES.DECIDEROTATION:
-                    let targetAngles = MathUtils.calculateAbsoluteAngles([this.targetX, this.targetY, this.targetZ]);
-                    let targetYaw = targetAngles.yaw;
+                    let lookingAt = Player.lookingAt();
+                    let isTargetCrop = false;
+
+                    if (lookingAt) {
+                        let block = World.getBlockAt(lookingAt.x, lookingAt.y, lookingAt.z);
+                        let registry = block?.type?.getRegistryName();
+
+                        isTargetCrop = Array.isArray(this.registry) ? this.registry.includes(registry) : registry === this.registry;
+                    }
+
+                    let targetYaw;
+
+                    if (isTargetCrop) {
+                        let currentBlockAngles = MathUtils.calculateAbsoluteAngles([lookingAt.x, lookingAt.y, lookingAt.z]);
+                        targetYaw = currentBlockAngles.yaw;
+                    } else {
+                        let targetAngles = MathUtils.calculateAbsoluteAngles([this.targetX, this.targetY, this.targetZ]);
+                        targetYaw = targetAngles.yaw;
+                    }
 
                     if (targetYaw > 180) targetYaw -= 360;
                     if (targetYaw <= -180) targetYaw += 360;
@@ -119,11 +174,111 @@ class FarmingMacro extends ModuleBase {
 
                     this.yaw = snappedYaw;
 
-                    Rotations.rotateToAngles(this.yaw, this.pitch);
-                    this.state = this.STATES.MOVEMENT;
+                    Rotations.rotateToAngles(this.yaw, this.pitch, 0.1);
+                    Rotations.onEndRotation(() => (this.state = this.STATES.DECIDEITEM));
                     break;
-                case this.STATES.MOVEMENT:
-                    // this.scan3x3x3(); //
+                case this.STATES.DECIDEITEM:
+                    let looking = Player.lookingAt();
+
+                    if (!looking) {
+                        // rare case
+                        this.message('&cYou are not looking at a crop!');
+                        this.toggle(false);
+                        return;
+                    }
+
+                    let block = World.getBlockAt(looking.x, looking.y, looking.z);
+                    let registry = block?.type?.getRegistryName();
+
+                    const cropTools = {
+                        'minecraft:nether_wart': 'Nether Wart Hoe',
+                        'minecraft:potatoes': 'Potato Hoe',
+                        'minecraft:wheat': 'Wheat Hoe',
+                        'minecraft:carrots': 'Carrot Hoe',
+                        'minecraft:melon': 'Melon Dicer',
+                        'minecraft:carved_pumpkin': 'Pumpkin Dicer',
+                        // fungi
+                        // dead rose or smth
+                        // the other two
+                    };
+
+                    let requiredToolName = cropTools[registry];
+
+                    if (!requiredToolName) {
+                        this.message(`&cNo tool mapped for block: ${registry}`);
+                        this.toggle(false);
+                        break;
+                    }
+
+                    let targetSlot = Guis.findItemInHotbar(requiredToolName);
+
+                    if (targetSlot !== -1) {
+                        Guis.setItemSlot(targetSlot);
+                        this.message(`Found ${requiredToolName} in slot ${targetSlot}`);
+                        if (Player.getHeldItemIndex() === targetSlot) this.state = this.STATES.DECIDEMOVEMENT;
+                    } else {
+                        this.message(`&cRequired tool "${requiredToolName}" not found in hotbar!`);
+                        this.toggle(false);
+                    }
+
+                    break;
+                case this.STATES.DECIDEMOVEMENT:
+                    if (this.name === 'Vertical NetherWart / Potato / Wheat / Carrot') {
+                        Keybind.setKey('leftclick', true);
+                        let scan = this.scanSides();
+
+                        let maxDistLeft = 0;
+                        let maxDistRight = 0;
+
+                        scan.forEach((block) => {
+                            let diff = block.offset;
+                            let distance = Math.abs(diff);
+
+                            if (block.name && !block.name.includes('air') && !block.name.includes('water')) {
+                                if (diff < 0) {
+                                    if (distance > maxDistLeft) maxDistLeft = distance;
+                                } else if (diff > 0) {
+                                    if (distance > maxDistRight) maxDistRight = distance;
+                                }
+                            }
+                        });
+
+                        if (maxDistRight > maxDistLeft) {
+                            this.message(`Wall detected on RIGHT. Moving LEFT to avoid!`);
+                            this.movementKey = 'a';
+                            this.ignoreKeys = ['d', 's'];
+                        } else if (maxDistLeft > maxDistRight) {
+                            this.message(`Wall detected on LEFT. Moving RIGHT to avoid!`);
+                            this.movementKey = 'd';
+                            this.ignoreKeys = ['a', 's'];
+                        } else {
+                            // check the crop metadata here then smh
+                        }
+
+                        this.state = this.STATES.IDLECHECKS;
+                    }
+                    break;
+                case this.STATES.IDLECHECKS:
+                    if (this.name === 'Vertical NetherWart / Potato / Wheat / Carrot') {
+                        Keybind.setKey('leftclick', true);
+                        Keybind.setKey(this.movementKey, true);
+                        this.ignoreKeys.forEach((key) => Keybind.setKey(key, false));
+
+                        let isOnGround = Player.asPlayerMP().isOnGround();
+
+                        if (!isOnGround) {
+                            ChatLib.chat('&cNot on ground!');
+                            Keybind.stopMovement();
+                            this.inAir = true;
+                        }
+
+                        if (this.inAir && isOnGround) {
+                            this.message('&aLanded! Recalculating movement...');
+
+                            this.inAir = false;
+                            this.state = this.STATES.DECIDEMOVEMENT;
+                        }
+                    }
                     break;
             }
         });
@@ -131,7 +286,7 @@ class FarmingMacro extends ModuleBase {
 
     scan3x3x3() {
         const playerBlockX = Math.floor(Player.getPlayer().getX());
-        const playerBlockY = Math.floor(Player.getPlayer().getY());
+        const playerBlockY = Math.round(Player.getPlayer().getY());
         const playerBlockZ = Math.floor(Player.getPlayer().getZ());
 
         const scanResults = [];
@@ -160,6 +315,43 @@ class FarmingMacro extends ModuleBase {
         return scanResults;
     }
 
+    scanSides() {
+        const playerBlockX = Math.floor(Player.getPlayer().getX());
+        const playerBlockY = Math.round(Player.getPlayer().getY());
+        const playerBlockZ = Math.floor(Player.getPlayer().getZ());
+
+        let yaw = Player.getYaw();
+        while (yaw < 0) yaw += 360;
+        yaw %= 360;
+
+        const scanResults = [];
+        const range = [-5, -4, -3, -2, -1, 1, 2, 3, 4, 5];
+
+        const facingNorthSouth = yaw <= 45 || (yaw >= 135 && yaw <= 225) || yaw >= 315;
+
+        for (const offset of range) {
+            let scanX = playerBlockX;
+            let scanZ = playerBlockZ;
+
+            if (facingNorthSouth) {
+                scanX = playerBlockX + offset;
+            } else {
+                scanZ = playerBlockZ + offset;
+            }
+
+            const block = World.getBlockAt(scanX, playerBlockY, scanZ);
+
+            scanResults.push({
+                x: scanX,
+                y: playerBlockY,
+                z: scanZ,
+                name: block?.type?.getRegistryName(),
+                offset: offset,
+            });
+        }
+
+        return scanResults;
+    }
     message(msg) {
         Chat.message('&#33ba11Farming Macro: &f' + msg);
     }
@@ -175,6 +367,8 @@ class FarmingMacro extends ModuleBase {
         global.macrostate.setMacroRunning(false, 'FARMING');
         Mouse.regrab();
         Rotations.stopRotation();
+        this.movementKey = null;
+        this.ignoreKey = null;
         this.message('&cDisabled');
         this.state = this.STATES.WAITING;
         this.farmAxis = null;
@@ -182,42 +376,3 @@ class FarmingMacro extends ModuleBase {
 }
 
 new FarmingMacro();
-
-// ig this could be improved somehow
-const FARMING_DATA = [
-    {
-        name: 'Vertical Netherwart',
-        registry: 'minecraft:nether_wart',
-        item: 'Nether Warts Hoe',
-        avgBPS: 2.19,
-        pitch: 3,
-    },
-    {
-        name: 'Vertical Potato',
-        registry: 'minecraft:potatoes',
-        item: 'Pythagorean Potato Hoe',
-        avgBPS: 2.19,
-        pitch: 3,
-    },
-    {
-        name: 'Vertical Wheat',
-        registry: 'minecraft:wheat',
-        item: "Euclid's Wheat Hoe",
-        avgBPS: 2.19,
-        pitch: 3,
-    },
-    {
-        name: 'Vertical Carrot',
-        registry: 'minecraft:carrots',
-        item: 'Gauss Carrot Hoe',
-        avgBPS: 2.19,
-        pitch: 3,
-    },
-    {
-        name: 'Melon/Pumpkin',
-        registry: ['minecraft:melon', 'minecraft:carved_pumpkin'],
-        item: 'Pumpkin Dicer' || 'Melon Dicer',
-        avgBPS: 2.19,
-        pitch: -59.2,
-    },
-];
