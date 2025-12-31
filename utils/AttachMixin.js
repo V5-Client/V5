46679de2680d952fcac1728b833c31c3312dea83
qxionr <dontemailme@stop.com>
2025-12-31 13:51:43 +0000

    fix attachmixin

--- utils/AttachMixin.js
+++ utils/AttachMixin.js
@@ -1,12 +1,16 @@
-//import { showNotification } from '../gui/NotificationManager';
-
-// Has to be in a seperate file so no error ;l
-
 export function attachMixin(mixin, name, callback) {
     try {
         mixin.attach(callback);
-        java.lang.System.out.println('V5: Mixin attached: ' + name);
+        print('V5: Mixin attached: ' + name);
     } catch (e) {
-        //showNotification(`Failed to attach ${name}`, e, 'ERROR');
+        Client.getMinecraft().execute(() => {
+            const NotificationManager = require('../gui/NotificationManager');
+
+            if (NotificationManager && NotificationManager.showNotification) {
+                NotificationManager.showNotification(`Failed to attach ${name}`, e, 'ERROR');
+            } else {
+                console.error(`Failed to attach ${name}: ${e}`);
+            }
+        });
     }
 }
