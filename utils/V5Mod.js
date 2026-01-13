909aef37a73e98da8760d533f3044857102947dd
EpsilonPhoenix <dontemailme@stop.com>
2026-01-12 22:42:00 -0500

    make welcomescreen show up once, then never again (until V5Config deleted)

--- utils/V5Mod.js
+++ utils/V5Mod.js
@@ -1,4 +1,5 @@
 import { Chat } from './Chat';
+import { Utils } from './Utils';
 
 const Fabric = net.fabricmc.loader.api.FabricLoader;
 
@@ -8,10 +9,24 @@ class V5Mod {
             Chat.message('&cV5 Mod is not installed!');
             // close minecraft, reload minecraft and install the mod
             // unload ct for now
-            UnloadCT(true);
-        } /*else {
-            Chat.message('&aV5 Mod is installed!');
-        }*/
+            if (typeof UnloadCT !== 'undefined') UnloadCT(true);
+        } else {
+            this.handleWelcome();
+        }
+    }
+
+    static handleWelcome() {
+        const META_FILE = 'v5_metadata.json';
+        const config = Utils.getConfigFile(META_FILE);
+
+        if (!config || !config.welcomeShown) {
+            try {
+                const WelcomeScreen = Java.type('com.v5.screen.WelcomeScreen');
+
+                WelcomeScreen.open();
+                Utils.writeConfigFile(META_FILE, { welcomeShown: true });
+            } catch (e) {}
+        }
     }
 }
 
