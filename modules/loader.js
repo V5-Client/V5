82c1938fd02782bc1c7e398afc35b167056c0c54
EpsilonPhoenix <129551402+EpsilonPhoenix@users.noreply.github.com>
2026-01-22 16:16:44 -0500

    loader

--- modules/loader.js
+++ modules/loader.js
@@ -22,7 +22,6 @@ import './visuals/BlockVisual.js';
 import './visuals/ESP.js';
 import './visuals/GIF.js';
 import './visuals/MobHider.js';
-import './visuals/Visual.js';
 import './visuals/Xray.js';
 
 /* SKILLS */
