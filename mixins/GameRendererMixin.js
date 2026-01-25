05657f786843bf1e569c9ea68d3e16e75021749b
qxionr <118845758+qxionr@users.noreply.github.com>
2026-01-25 21:09:29 +0000

    remove not needed

--- mixins/GameRendererMixin.js
+++ mixins/GameRendererMixin.js
@@ -1,8 +1,4 @@
-import { attachMixin } from '../utils/AttachMixin';
 import { Mixin } from '../utils/MixinManager';
-
-const gameRendererMixin = new Mixin('net.minecraft.client.render.GameRenderer');
-const Window = new Mixin('net.minecraft.client.util.Window');
 
 Mixin('net.minecraft.client.render.GameRenderer')
     .modifyExpressionValue({
